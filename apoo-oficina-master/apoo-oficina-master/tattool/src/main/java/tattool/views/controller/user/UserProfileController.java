00c985ebe9f7d8661fe93a53d1d266cbb4b977b6
João Goulart <jv.goulart.almeida@hotmail.com>
2019-10-26 14:28:03 -0300

    C3 Dados de perfil que não estão sendo exibidos

--- apoo-oficina-master/apoo-oficina-master/tattool/src/main/java/tattool/views/controller/user/UserProfileController.java
+++ apoo-oficina-master/apoo-oficina-master/tattool/src/main/java/tattool/views/controller/user/UserProfileController.java
@@ -6,6 +6,8 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import tattool.rest.consume.UserRest;
+import tattool.util.UserSession;
 
 public class UserProfileController implements Initializable{
 
@@ -17,12 +19,14 @@ public class UserProfileController implements Initializable{
 
     @FXML
     private Label userFunction;
+    
+    private UserRest rest = new UserRest();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		userName.setText("<Nome do usuario>");
-		userLogin.setText("<Login do usuario>");
-		userFunction.setText("<Funcao do usuario>");
+		userName.setText(UserSession.userLogado.getNome());
+		userLogin.setText(UserSession.userLogado.getUsuario());
+		userFunction.setText(UserSession.userLogado.getRole() == 1 ? "Admin" : "Funcionário");
 	}
 
 }
